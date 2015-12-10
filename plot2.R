a8c03f569c2c475ced212a8cfb26b604c0e9c868
CSoaresF <csoaresf@yahoo.com>
2015-12-09 21:37:46 -0300

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -10,7 +10,10 @@ if(!file.exists("data/household_power_consumption.txt")) {
     unzip(zipfile="data.zip", exdir="data")
 }
 # load file to workspace
-data1 <- read.table("C:/EDA_PROJECT1/data/household_power_consumption.txt", header=TRUE, sep=";", dec=".", stringsAsFactors=FALSE)
+data1 <- read.table("C:/EDA_PROJECT1/data/household_power_consumption.txt", 
+                    header=TRUE, 
+                    sep=";", dec=".", 
+                    stringsAsFactors=FALSE)
 
 # filter days 1 and 2 february 2007 
 data2<- subset(data1, (data1$Date == "1/2/2007" | data1$Date== "2/2/2007")) 
@@ -21,5 +24,10 @@ data2$DateTime <- as.POSIXct(paste(data2$Date, data2$Time))
 
 # generate graphic
 png("plot2.png", width=480, height= 480)
-plot(data2$DateTime, data2$Global_active_power, type= "l", lwd=1, ylab= "Global Active Power (kilowatts)", xlab="")
+plot(data2$DateTime, 
+     data2$Global_active_power, 
+     type= "l", 
+     lwd=1, 
+     ylab= "Global Active Power (kilowatts)", 
+     xlab="")
 dev.off()
